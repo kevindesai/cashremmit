57f29f14216851b57d63bf73b4591df4f06937c9
Sanket <Sanket@Sanket-VAIO>
2016-07-02 08:21:11 +0530

    user server side validation

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,22 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
 use App\User;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Session;
+use Validator;
 
-class UsersController extends Controller
-{
+class UsersController extends Controller {
+
     /**
      * Display a listing of the resource.
      *
      * @return void
      */
-    public function index()
-    {
-        
+    public function index() {
+
         $users = User::paginate(15);
         return view('users.index', compact('users'));
     }
@@ -29,8 +28,7 @@ class UsersController extends Controller
      *
      * @return void
      */
-    public function create()
-    {
+    public function create() {
         return view('users.create');
     }
 
@@ -39,9 +37,24 @@ class UsersController extends Controller
      *
      * @return void
      */
-    public function store(Request $request)
-    {
-        
+    public function store(Request $request) {
+//        echo $request->first_name;die;
+        $validation = Validator::make(
+                        array(
+                    'first_name' => $request->first_name,
+                    'last_name' => $request->last_name,
+                    'email' => $request->email,
+                        ), array(
+                    'first_name' => array('required', 'alpha_dash'),
+                    'last_name' => array('required', 'alpha_dash'),
+                    'email' => array('required', 'email'),
+                        )
+        );
+
+        if ($validation->fails()) {
+            return $validation->messages();
+        }
+
         User::create($request->all());
 
         Session::flash('flash_message', 'User added!');
@@ -56,8 +69,7 @@ class UsersController extends Controller
      *
      * @return void
      */
-    public function show($id)
-    {
+    public function show($id) {
         $user = User::findOrFail($id);
 
         return view('users.show', compact('user'));
@@ -70,8 +82,7 @@ class UsersController extends Controller
      *
      * @return void
      */
-    public function edit($id)
-    {
+    public function edit($id) {
         $user = User::findOrFail($id);
 
         return view('users.edit', compact('user'));
@@ -84,9 +95,8 @@ class UsersController extends Controller
      *
      * @return void
      */
-    public function update($id, Request $request)
-    {
-        
+    public function update($id, Request $request) {
+
         $user = User::findOrFail($id);
         $user->update($request->all());
 
@@ -102,12 +112,12 @@ class UsersController extends Controller
      *
      * @return void
      */
-    public function destroy($id)
-    {
+    public function destroy($id) {
         User::destroy($id);
 
         Session::flash('flash_message', 'User deleted!');
 
         return redirect('users');
     }
+
 }
