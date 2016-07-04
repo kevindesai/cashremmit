a07ccd551aa14b9cfd095b8430f8cbbec60894b7
Sanket <Sanket@Sanket-VAIO>
2016-07-04 22:21:40 +0530

    Currency convert api https://www.getpostman.com/collections/1616596d73df48bfa20e

--- app/Http/Controllers/API/CurrencyAPIController.php
+++ app/Http/Controllers/API/CurrencyAPIController.php
@@ -15,7 +15,7 @@ use Carbon\Carbon;
 use Session;
 use Response;
 use Validator;
-
+use Exception;
 class CurrencyAPIController extends Controller {
 
     public function Convert(Request $request) {
@@ -38,17 +38,29 @@ class CurrencyAPIController extends Controller {
         }
         foreach ($input as $k => $v)
             $$k = $v;
-
-        $data = file_get_contents("https://www.google.com/finance/converter?a=$amount&from=$from&to=$to");
-        preg_match("/<span class=bld>(.*)<\/span>/", $data, $converted);
-        $converted = preg_replace("/[^0-9.]/", "", $converted[1]);
+        try {
+            $data = file_get_contents("https://www.google.com/finance/converter?a=$amount&from=$from&to=$to");
+            preg_match("/<span class=bld>(.*)<\/span>/", $data, $converted);
+            if (isset($converted[1]))
+                $converted = preg_replace("/[^0-9.]/", "", $converted[1]);
+            else
+                throw new Exception("Error");
+                $response = array(
+                'status' => '1',
+                'original' => number_format(round($amount, 3), 2),
+                'converted' => number_format(round($converted, 3), 2),
+                'message' => 'Success'
+            );
+        } catch (Exception $e) {
+            $response = array(
+                'status' => '0',
+                'original' => number_format(round($amount, 3), 2),
+                'converted' => number_format(round(0, 3), 2),
+                'message' => 'Not Converted'
+            );
+        }
 //        return number_format(round($converted, 3), 2);
-        $response = array(
-            'status' => '1',
-            'original' => number_format(round($amount, 3), 2),
-            'converted' => number_format(round($converted, 3), 2),
-            'message' => 'Success'
-        );
+
         return json_encode($response);
     }
 
